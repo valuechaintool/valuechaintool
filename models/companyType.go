e2afae18ce535a5a1e8e74446d493792340e7d7d
Fabio Alessandro Locati <me@fale.io>
2020-05-04 14:52:32 +0200

    CompanyType is now more flexible

--- models/companyType.go
+++ models/companyType.go
@@ -1,8 +1,67 @@
 package models
 
-type CompanyType int
+import (
+	"fmt"
+	"time"
 
-const (
-	PartnerCompany CompanyType = 1 + iota
-	CustomerCompany
+	"github.com/google/uuid"
 )
+
+type CompanyType struct {
+	ID           uuid.UUID  `json:"id" sql:"primary key" gorm:"type:uuid"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
+	DeletedAt    *time.Time ``
+	Name         string     `json:"name"`
+	Abbreviation string     `json:"abbreviation"`
+}
+
+func (t *CompanyType) BeforeSave() error {
+	if err := t.Validate(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *CompanyType) Conflicts() bool {
+	return false
+}
+
+func (t *CompanyType) Validate() error {
+	return nil
+}
+
+func (t *CompanyType) Save() error {
+	return session.Save(t).Error
+}
+
+func (t *CompanyType) Delete() error {
+	if t.ID == uuid.Nil {
+		return fmt.Errorf("missing Primary Key")
+	}
+	return session.Delete(t).Error
+}
+
+func NewCompanyType(t *CompanyType) error {
+	if t.ID == uuid.Nil {
+		var err error
+		if t.ID, err = uuid.NewRandom(); err != nil {
+			return err
+		}
+	}
+	if t.Conflicts() {
+		return fmt.Errorf("the item already exists")
+	}
+	if err := session.Create(&t).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func ListCompanyTypes(filters map[string]interface{}) ([]CompanyType, error) {
+	var items []CompanyType
+	if err := session.Where(filters).Find(&items).Error; err != nil {
+		return nil, err
+	}
+	return items, nil
+}
