929a7dcb44e49055c783fe5ab4775ea4f7926fa8
Fabio Alessandro Locati <me@fale.io>
2020-05-22 09:42:57 +0200

    remove blank space

--- models/tier.go
+++ models/tier.go
@@ -5,7 +5,7 @@ var tiers []Tier
 type Tier struct {
 	ID    int    `mapstructure:"id" json:"id"`
 	Name  string `mapstructure:"name" json:"name"`
-	Color string `mapstructure: "color" json:"color"`
+	Color string `mapstructure:"color" json:"color"`
 }
 
 func ListTiers() []Tier {
